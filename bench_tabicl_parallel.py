de4bde6b2f0565da2caef5b626f06c583dafd265
hao405 <1815526897@qq.com>
2026-03-21 10:50:19 +0800

    a

--- bench_tabicl_parallel.py
+++ bench_tabicl_parallel.py
@@ -488,7 +488,7 @@ def main(argv=None):
     p = argparse.ArgumentParser(description='Parallel Benchmark TabICLClassifier on TALENT datasets')
     p.add_argument('--model-path', default=None, help='Path to TabICL checkpoint')
     p.add_argument('--data-root', default='data181', help='Root path to TALENT data folder')
-    p.add_argument('--outdir', default='tabiclv2_ensmble32', help='Directory to save results')
+    p.add_argument('--outdir', default='tabiclv2_ensmble32_data181', help='Directory to save results')
     p.add_argument('--max-datasets', type=int, default=None, help='Limit number of datasets')
     p.add_argument('--verbose', action='store_true')
     p.add_argument('--n-estimators', type=int, default=32, help='Number of estimators for the ensemble')
